1d8f11dde3ba79ea667de834db006864f050b28a
kedzkiest <66341676+kedzkiest@users.noreply.github.com>
2022-01-02 12:07:45 +0900

    Chapter5 completed.

---
+++ Navigation Meshes/ChasePlayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour
+{
+    private GameObject player;
+
+    private NavMeshAgent agent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
